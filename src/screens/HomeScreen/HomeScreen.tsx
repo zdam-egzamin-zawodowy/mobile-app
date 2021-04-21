33bf925ea8c77958d678e0281fe028311633ff3f
Kichiyaki <xyztojajestem@gmail.com>
2021-04-21 19:46:04 +0200

    remove unnecessary permissions, use useEffect instead of useMemo for filtering professions

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -2,7 +2,6 @@ import React, { useMemo, useState } from 'react';
 import { NetworkStatus, useQuery } from '@apollo/client';
 import { useUpdateEffect } from 'react-use';
 import { useVariables } from 'libs/native-base';
-import { useSavedQualifications } from 'libs/savedqualifications';
 import { Query, QueryProfessionsArgs } from 'libs/graphql';
 import { EMAIL } from 'config/app';
 import { QUERY_PROFESSIONS } from './queries';
@@ -18,7 +17,6 @@ const HomeScreen = () => {
   const [search, setSearch] = useState('');
   const [mode, setMode] = useState(Mode.All);
   const variables = useVariables();
-  const { isSaved } = useSavedQualifications();
   const { loading, data, refetch, networkStatus, error } = useQuery<
     Pick<Query, 'professions'>,
     QueryProfessionsArgs
@@ -32,23 +30,6 @@ const HomeScreen = () => {
       profession => profession.qualifications.length > 0,
     );
   }, [data]);
-  const filteredProfessions = useMemo(() => {
-    if (!search && mode === Mode.All) {
-      return professions;
-    }
-    return professions
-      .map(profession => ({
-        ...profession,
-        qualifications: profession.qualifications.filter(
-          qualification =>
-            (!search ||
-              qualification.name.toLowerCase().includes(search) ||
-              qualification.code.toLowerCase().includes(search)) &&
-            (mode === Mode.All || isSaved(qualification.id)),
-        ),
-      }))
-      .filter(profession => profession.qualifications.length > 0);
-  }, [search, professions, mode, isSaved]);
   useUpdateEffect(() => {
     if (error && error.networkError) {
       Alert.alert(
@@ -75,9 +56,11 @@ const HomeScreen = () => {
         </Content>
       ) : (
         <Professions
-          professions={filteredProfessions}
+          professions={professions}
           refreshing={networkStatus === NetworkStatus.refetch}
           onRefresh={refetch}
+          mode={mode}
+          search={search}
         />
       )}
     </Container>
--- src/screens/HomeScreen/components/Professions/Professions.tsx
+++ src/screens/HomeScreen/components/Professions/Professions.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useRef, useState } from 'react';
 import { useUpdateEffect } from 'react-use';
+import { useSavedQualifications } from 'libs/savedqualifications';
 import { Maybe, Profession, Qualification } from 'libs/graphql';
+import { Mode } from '../ModeSelector/ModeSelector';
 
 import { FlatListProps, RefreshControl, StyleSheet } from 'react-native';
 import { List, View } from 'native-base';
@@ -11,21 +13,52 @@ import ListEmpty from './ListEmpty';
 export interface ProfessionsProps
   extends Pick<FlatListProps<Profession>, 'refreshing' | 'onRefresh'> {
   professions: Profession[];
+  mode: Mode;
+  search: string;
 }
+
+const noop = () => {};
+
 const Professions = ({
   professions,
   refreshing,
   onRefresh,
+  mode,
+  search,
 }: ProfessionsProps) => {
   const listRef = useRef<any>(null);
+  const { isSaved } = useSavedQualifications();
+  const [filteredProfessions, setFilteredProfessions] = useState<
+    Maybe<Profession[]>
+  >(null);
   const [selectedQualification, setSelectedQualification] = useState<
     Maybe<Qualification>
   >(null);
   const [showModal, setShowModal] = useState(false);
-
   useUpdateEffect(() => {
     listRef.current?._root?.scrollToOffset({ offset: 0, animated: false });
-  }, [professions]);
+  }, [mode]);
+  useUpdateEffect(() => {
+    if (!search && mode === Mode.All && filteredProfessions) {
+      setFilteredProfessions(null);
+      return;
+    }
+
+    const newFilteredProfessions: Profession[] = [];
+    professions.forEach(profession => {
+      const qualifications = profession.qualifications.filter(
+        qualification =>
+          (!search ||
+            qualification.name.toLowerCase().includes(search) ||
+            qualification.code.toLowerCase().includes(search)) &&
+          (mode === Mode.All || isSaved(qualification.id)),
+      );
+      if (qualifications.length > 0) {
+        newFilteredProfessions.push({ ...profession, qualifications });
+      }
+    });
+    setFilteredProfessions(newFilteredProfessions);
+  }, [professions, search, mode, isSaved]);
 
   const handlePress = useCallback(
     (qualification: Qualification) => {
@@ -46,7 +79,7 @@ const Professions = ({
     <View style={styles.container}>
       <List
         ref={listRef}
-        dataArray={professions}
+        dataArray={filteredProfessions ?? professions}
         contentContainerStyle={styles.contentContainer}
         renderItem={renderItem}
         ListEmptyComponent={<ListEmpty />}
@@ -55,7 +88,7 @@ const Professions = ({
         refreshControl={
           <RefreshControl
             refreshing={refreshing ?? false}
-            onRefresh={onRefresh ?? (() => {})}
+            onRefresh={onRefresh ?? noop}
           />
         }
         initialNumToRender={5}
